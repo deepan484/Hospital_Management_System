b716b9df816f32ba5ddedf7c763f69341ad0817d
deepan484 <141907436+deepan484@users.noreply.github.com>
2024-12-07 18:45:42 +0530

    Delete Hosipital Management System directory

--- Hosipital Management System/database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_db_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="hospital",
-        user="postgres",
-        password="g0916032p"
-    )
